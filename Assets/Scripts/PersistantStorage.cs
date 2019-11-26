5a9ac10c6507d10cf51bed3d6a4498c1723ae134
Bailee Vaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-11-26 14:40:06 +1100

    Initial commit

---
+++ Assets/Scripts/PersistantStorage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PersistantStorage : MonoBehaviour
+{
+    string savePath;
+
+    private void Awake()
+    {
+        savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+    }
+
+    public void Save(PersistableObject pO)
+    {
+        using (var writer = new BinaryWriter(File.Open(savePath, FileMode.Create)))
+        {
+            pO.Save(new GameDataWriter(writer));
+        }
+    }
+    public void Load(PersistableObject pO)
+    {
+        using (var reader = new BinaryReader(File.Open(savePath, FileMode.Open)))
+        {
+            pO.Load(new GameDataReader(reader));
+        }
+    }
+}
